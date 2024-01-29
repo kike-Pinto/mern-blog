19c4c01814c20211e0ccd1dab0eb93729d1d511c
kike-pinto <cripintort@gmail.com>
2024-01-29 19:26:52 -0300

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config()
 
@@ -15,9 +16,8 @@ mongoose
     console.log(err)
   })
 
-// mongodb+srv://cripintort:AKtaBtgnrd2vAeK5@mern-blog-2.zrbnuea.mongodb.net/?retryWrites=true&w=majority
-// AKtaBtgnrd2vAeK5
-
 app.listen(3000, () => {
   console.log('Server in running on port 3000!!!')
 })
+
+app.use('/api/user', userRoutes)
