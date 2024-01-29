1cbc0106f6940308fac06191281cac4b1e7c5426
kike-pinto <cripintort@gmail.com>
2024-01-29 18:55:04 -0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+// mongodb+srv://cripintort:AKtaBtgnrd2vAeK5@mern-blog-2.zrbnuea.mongodb.net/?retryWrites=true&w=majority
+// AKtaBtgnrd2vAeK5
 
 app.listen(3000, () => {
   console.log('Server in running on port 3000!!!')
