5773a96787bf5dbaf6791459362b3fe066537b38
kike-pinto <cripintort@gmail.com>
2024-02-01 15:27:03 -0300

    complete dark mode functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -45,10 +45,10 @@ const SignIn = () => {
   }
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-6'>
         {/* left side */}
         <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-5xl'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
               Kikend&apos;s
             </span>
