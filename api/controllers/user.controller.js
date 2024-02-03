e25447eec379ed8b9b2b1063d3bad58a31d51343
kike-pinto <cripintort@gmail.com>
2024-02-03 16:57:25 -0300

    complete profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,7 +8,7 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are nor allowed to update this user'))
+    return next(errorHandler(403, 'You are not allowed to update this user'))
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
@@ -17,7 +17,7 @@ export const updateUser = async (req, res, next) => {
     req.body.password = bcryptjs.hashSync(req.body.password, 10)
   }
   if (req.body.username) {
-    if (req.body.username.length < 7 || req.body.username > 20) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
         errorHandler(400, 'Username must be between 7 and 20 characters')
       )
@@ -33,23 +33,23 @@ export const updateUser = async (req, res, next) => {
         errorHandler(400, 'Username can only contain letters and numbers')
       )
     }
-    try {
-      const updateUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      )
-      const { password, ...rest } = updateUser._doc
-      res.status(200).json(rest)
-    } catch (error) {
-      next(error)
-    }
+      },
+      { new: true }
+    )
+    const { password, ...rest } = updatedUser._doc
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
   }
 }
